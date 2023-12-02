3d979117ea068d8d4fa8327b317e409431f3a10e
WowLinux <jhjeong@wowlinux.co.kr>
2023-12-02 15:07:59 +0900

    게시물 ㄹㅣ스트 기능구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -1,4 +1,6 @@
 package com.koreaIT.java.BAM;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +10,9 @@ public class Main {
 		Scanner sc=new Scanner(System.in);
 		
 		int lastArticleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
+		
 		while(true) {
 			System.out.printf("명령어)");
 			String cmd = sc.nextLine().trim();
@@ -25,21 +30,40 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");			
 				String body = sc.nextLine();
-				System.out.printf("%d번 글이 생성되었습니다\n",id);
-//				System.out.printf("%s, %s\n, title, body");
- 
+				
+				Article article = new Article(id,title,body);
+				articles.add(article);
+						
+				System.out.printf("%d번 글이 생성되었습니다\n",id); 
 			}else if (cmd.equals("article list")) {
-				System.out.println("게시물이 없습니다.");				
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}				
+				System.out.println("번호	|	제목");
+				for(int i = articles.size()-1; i >= 0; i--) {
+					Article article=articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);					
+				}
+				
 			} else { 
 				System.out.println("존재하지 않는 명령어 입니다.");				
-			}
+			}	 
 		}
 		
-		System.out.println("==프로그램 끝");
-		
-		sc.close();
-		
+		System.out.println("==프로그램 끝");		
+		sc.close();		
 	}
-
+}
+class Article{
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id=id;
+		this.title=title;
+		this.body=body;		
+	}	
 }
 
