bff05a53b9c5cd3d680058838698c2fa87db8ef8
WowLinux <jhjeong@wowlinux.co.kr>
2023-12-06 21:36:09 +0900

    조회수 기능구현, list,detail에 정보 추가

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -42,10 +42,10 @@ public class Main {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}				
-				System.out.println("번호	|	제목	|	날짜	");
+				System.out.println("번호	|	제목	|	날짜	|	조회수");
 				for(int i = articles.size()-1; i >= 0; i--) {
 					Article article=articles.get(i);
-					System.out.printf("%d	|	%s	|	%s	\n", article.id, article.title, article.regDate);					
+					System.out.printf("%d	|	%s	|	%s	|	%d\n", article.id, article.title, article.regDate, article.viewCnt);					
 				}
 				
 			} else if(cmd.startsWith("article detail ")) {
@@ -56,7 +56,7 @@ public class Main {
 				Article foundArticle = null;				
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
-					if(article.id == id) {	
+					if(article.id == id) {						
 						foundArticle = article;						
 						break;
 					}					
@@ -65,10 +65,12 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n",id);				
 					continue;					
 				}  
+				foundArticle.addViewCnt();
 				System.out.printf("번호 : %d\n",foundArticle.id);
 				System.out.printf("날짜 : %s\n",foundArticle.regDate);
 				System.out.printf("제목 : %s\n",foundArticle.title);
 				System.out.printf("내용 : %s\n",foundArticle.body);	 
+				System.out.printf("조회수 : %d\n",foundArticle.viewCnt);	 
 
 			} else if(cmd.startsWith("article modify ")) {
 			
@@ -136,12 +138,17 @@ class Article{
 	String regDate;
 	String title;
 	String body;
+	int viewCnt;
 	
 	Article(int id, String regDate,String title, String body){
 		this.id=id;
 		this.regDate=regDate;
 		this.title=title;
-		this.body=body;		
-	}	
+		this.body=body;	
+		this.viewCnt=0;
+ 	}	
+	public void addViewCnt() {
+		viewCnt++;		
+	}
 }
 
