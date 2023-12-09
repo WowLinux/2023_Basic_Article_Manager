a3b5abe5ded4fed6c656a565e49ea9b49b594202
WowLinux <jhjeong@wowlinux.co.kr>
2023-12-09 09:12:55 +0900

    게시물 article list에 검색기능 추가

--- src/com/koreaIT/java/BAM/App.java
+++ src/com/koreaIT/java/BAM/App.java
@@ -47,17 +47,35 @@ public class App {
 				articles.add(article);
 						
 				System.out.printf("%d번 글이 생성되었습니다\n",id); 
-			}else if (cmd.equals("article list")) {
+			}else if (cmd.startsWith("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
-				}				
-				System.out.println("번호	|	제목	|	날짜	|	조회수");
-				for(int i = articles.size()-1; i >= 0; i--) {
-					Article article=articles.get(i);
-					System.out.printf("%d	|	%s	|	%s	|	%d\n", article.id, article.title, article.regDate, article.viewCnt);					
 				}
 				
+				List<Article> forPrintArticles = articles;
+				String searchKeyword = cmd.substring("article list".length()).trim();
+		        
+				if(searchKeyword.length() > 0) {
+					System.out.println("검색어 : " + searchKeyword);
+					forPrintArticles = new ArrayList<>();
+					for(Article article : articles) {
+						if (article.title.contains(searchKeyword)){
+							forPrintArticles.add(article);							
+						}						
+					}
+					if(forPrintArticles.size() == 0) {
+						System.out.println("검색결과가 없습니다.");
+						continue;
+					}
+				}
+				
+				System.out.println("번호	|	제목	|	날짜	|	조회수");
+				for(int i = forPrintArticles.size()-1; i >= 0; i--) {
+					Article article=forPrintArticles.get(i);
+					System.out.printf("%d	|	%s	|	%s	|	%d\n", article.id, article.title, article.regDate, 
+							article.viewCnt);					
+				}				
 			} else if(cmd.startsWith("article detail ")) {
 				
 				String[] cmdBits = cmd.split(" ");
